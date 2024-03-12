fad339d32359885c18e8bc3cb40f3d3295495232
Massimo Nocentini <massimo.nocentini@gmail.com>
2024-03-12 13:55:09 +0100

    More tests.

--- src/fds-queue.scm
+++ src/fds-queue.scm
@@ -12,26 +12,36 @@
 	(define (fds-queue-car q)
 	  (cond
 	   ((fds-queue-empty? q) (error 'empty))
-	   (else (caar q))))
+	   (else (cadr q))))
  
-    (define (fds-queue-check q)
-	  (cond
-	   ((null? (car q)) (cons (reverse (cdr q)) '()))
-	   (else q)))
+    (define (fds-queue-check size front rear)
+		(cons size (cond
+					((null? front) (cons (reverse rear) '()))
+					(else (cons front rear)))))
  
+	(define-syntax letqueuefront&rear
+	  (syntax-rules ()
+	  	((_ () body ...) (begin body ...))
+		((_ (((size front rear) expr) ((ssize ffront rrear) eexpr) ...) body ...)
+		 (letcar&cdr (((size repr) expr))
+				(letcar&cdr (((front rear) repr))
+					(letqueuefront&rear (((ssize ffront rrear) eexpr) ...) body ...))))))
+
    	(define (fds-queue-cdr q)
 	  (cond
 	   ((fds-queue-empty? q) (error 'empty))
-	   (else (letcar&cdr (((a d) q))
-                (fds-queue-check (cons (cdr a) d))))))
+	   (else (letqueuefront&rear (((size front rear) q))
+				(fds-queue-check (sub1 size) (cdr front) rear)))))
  
-    (define (fds-queue-push q x)
-        (letcar&cdr (((a d) q)) (fds-queue-check (cons a (cons x d)))))
+    (define (fds-queue-cons x q)
+        (letqueuefront&rear (((size front rear) q))
+			(fds-queue-check (add1 size) front (cons x rear))))
 
 	(define-syntax letqueue
 	  (syntax-rules ()
 	  	((_ ((q expr) ...) body ...)
 		 (let ((q fds-queue-empty) ...)
-		 	(for-each (lambda (x) (set! q (fds-queue-push q x))) expr) ...
+		 	(for-each (lambda (x) (set! q (fds-queue-cons x q))) expr) ...
 			body ...))))
+
 )
--- src/test/queue.scm
+++ src/test/queue.scm
@@ -5,8 +5,16 @@
     
     ((test-empty _)
         (⊦= 2 (cadr '(1 2)))
-        (letqueue ((q '())) (⊦= #t (fds-queue-empty? q))))
-
+        (letqueue ((q '())
+                   (w '(a))
+                   (e '(a b))
+                   (r '(a b c))
+                   (t '(a b c d))) 
+            (⊦= '(0 ()) q)
+            (⊦= '(1 (a)) w)
+            (⊦= '(2 (a) b) e)
+            (⊦= '(3 (a) c b) r)
+            (⊦= '(4 (a) d c b) t)))
 )
 
 (unittest/✓ queue-suite)
